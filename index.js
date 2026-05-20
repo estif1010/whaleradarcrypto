ff786160e706643034f9e29bfc1af40f099f9ee7
estif1010 <ezewdu443@gmail.com>
2026-05-20 11:58:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 require('dotenv').config();
 
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot =
+require('node-telegram-bot-api');
 
 const bot = new TelegramBot(
-  process.env.BOT_TOKEN,
-  { polling: true }
+process.env.BOT_TOKEN,
+{ polling: true }
 );
 
+const CHANNEL_ID =
+process.env.CHANNEL_ID;
+
+// START
 bot.onText(/\/start/, (msg) => {
 
-  bot.sendMessage(
-    msg.chat.id,
-    '🐋 WhaleRadarCrypto Active'
-  );
+bot.sendMessage(
+msg.chat.id,
+'🐋 WhaleRadarCrypto Active'
+);
+
+});
+
+// TEST SIGNAL
+bot.onText(/\/test/, async (msg) => {
+
+await bot.sendMessage(
+CHANNEL_ID,
+`
+🚨 Whale Alert
+
+🐋 5,000 BTC moved
+
+📥 Binance
+💰 $320M
+`
+);
+
+bot.sendMessage(
+msg.chat.id,
+'✅ Signal Sent'
+);
 
 });
